a8801b564642ee14ee8ffe8c892c400c33a7dc68
Luis Escobar <lae311@stern.nyu.edu>
2021-10-18 15:58:03 -0400

    commit tree e75c4190b491de200e52cb31c047a751cb4b6184

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function (e){
+    const p = document.getElementById('text')
+    const newP = document.createElement('p');
+    newP.textContent = "This is really cool!";
+    e = p.parentNode.replaceChild(newP, p)
+})
